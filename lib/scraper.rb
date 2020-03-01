992587047b9e0b82e8f48e0d36a9f543042452b2
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-01 06:39:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,8 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
     binding.pry 
-    end 
   end 
+  
   
   Scraper.new.get_page
   
